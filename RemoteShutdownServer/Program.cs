077ba0ba849d8ed9dbf2d1f34641070317063f77
ngnhngngn <32558315+ngnhngngn@users.noreply.github.com>
2017-10-22 13:03:01 +0700

    LTM-buoi-4
    Nguyễn Hoàng Nguyên - 14DH110187

---
+++ RemoteShutdownServer/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace RemoteShutdownServer
+{
+	class Program
+	{
+		private static ShutdownServer server;
+		static void Main(string[] args)
+		{
+			server = new ShutdownServer();
+			server.start();
+			server.listening();
+		}
+	}
+}
